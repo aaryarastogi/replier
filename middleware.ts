fa8cf4020e3fb7d71e113ac5492787da6e29e697
Aarya Rastogi <97822457+aaryarastogi@users.noreply.github.com>
2025-03-19 20:36:36 +0530

    nextauth

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,9 @@ export default auth((req) => {
   const isDashboard = req.nextUrl.pathname.startsWith("/dashboard");
 
   if (!isLoggedIn && isDashboard) {
-    const loginUrl = new URL("/api/auth/callback/google", req.nextUrl.origin);
+    // Use the NEXTAUTH_URL environment variable to construct the login URL
+    const baseUrl = process.env.BASE_URL || req.nextUrl.origin;
+    const loginUrl = new URL("/api/auth/signin", baseUrl);
     loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
 
     return Response.redirect(loginUrl);
